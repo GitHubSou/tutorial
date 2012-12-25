cbe0c4f8ec4c1ab5e62171fd4db6939e0ade0fdc
sou <pai_3_14159265_year@yahoo.co.jp>
2012-12-25 20:13:15 +0900

    first

--- Option.java
+++ Option.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Option {
     long totalVolumeSize = 0; // 全体の容量のサイズf
-    int num = 0; // 単位の係数
+    int unitFactor = 0; // 単位の係数
     File outputFile;
 
     String fileName; // ファイル名を格納する
@@ -17,8 +17,8 @@ public class Option {
         // 第二引数から容量をバイトに変換する。 & ベース容量を定めておく
         for (int i = 0; i < 3; i++) {
             if (args[1].indexOf(unitNameList[i]) != -1) {
-                num = Integer.parseInt(args[1].substring(0, args[1].length() - 1));
-                totalVolumeSize = (long) Math.pow(2, 10 * (i + 1)) * num;
+                unitFactor = Integer.parseInt(args[1].substring(0, args[1].length() - 1));
+                totalVolumeSize = (long) Math.pow(2, 10 * (i + 1)) * unitFactor;
             }
         }
         // 第三引数から分割する数を所得する。
